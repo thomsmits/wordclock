a6a1109471f31a7f94586b440fe41cf7753d68fd
Daniel Springwald <Springwald@users.noreply.github.com>
2020-02-06 23:18:29 +0100

    added 2012 layout

--- neoclock/hardware.h
+++ neoclock/hardware.h
@@ -11,6 +11,9 @@
 /* Value to dim the stripe in general */
 double dim = 1.0;
 
+#define LAYOUT_2012
+#undef LAYOUT_2020
+
 /* Maximum values for hour and minute */
 #define MAX_HOUR 23
 #define MAX_MINUTE 59
@@ -59,6 +62,9 @@ Color palette[PALETTE_SIZE] = {
   { 0x1f, 0x00, 0x00 },
 };
 
+
+
+
 /**
   * Positions of the text on the LED stripe.
   *
@@ -69,36 +75,73 @@ Color palette[PALETTE_SIZE] = {
   *
   * The range of the values in the array have to be < NUMBER_OF_LEDS.
  */
-const uint8_t ES[]          = { 103, 104, 0xff };
-const uint8_t IST[]         = { 106, 107, 108, 0xff };
-const uint8_t FUENF_M[]     = { 110, 111, 112, 113, 0xff };
-const uint8_t ZEHN_M[]      = { 102, 101, 100, 99, 0xff };
-const uint8_t ZWANZIG[]     = { 98, 97, 96, 95, 94, 93, 92, 0xff };
-const uint8_t DREIVIERTEL[] = { 81, 82, 83, 84, 85, 86, 87, 88, 89 ,90, 91, 0xff };
-const uint8_t VIERTEL[]     = { 85, 86, 87, 88, 89, 90, 91, 0xff };
-const uint8_t VOR[]         = { 74, 73, 72, 0xff };
-const uint8_t NACH[]        = { 78, 77, 76, 75, 0xff };
-const uint8_t HALB[]        = { 59, 60, 61, 62, 0xff };
-const uint8_t ELF[]         = { 34, 35, 36, 0xff };
-const uint8_t EIN[]         = { 56, 55, 54, 0xff };
-const uint8_t EINS[]        = { 56, 55, 54, 53, 0xff };
-const uint8_t ZWEI[]        = { 58, 57, 56, 55, 0xff };
-const uint8_t DREI[]        = { 38, 39, 40, 41, 0xff };
-const uint8_t FUENF[]       = { 44, 45, 46, 47, 0xff };
-const uint8_t VIER[]        = { 26, 27, 28, 29, 0xff };
-const uint8_t SECHS[]       = { 9, 10, 11, 12, 13, 0xff };
-const uint8_t ACHT[]        = { 16, 17, 18, 19, 0xff };
-const uint8_t SIEBEN[]      = { 53, 52, 51, 50, 49, 48, 0xff };
-const uint8_t ZWOELF[]      = { 64, 65, 66, 67, 68, 0xff };
-const uint8_t ZEHN[]        = { 20, 21, 22, 23, 0xff };
-const uint8_t UHR[]         = { 4, 5, 6, 0xff };
-const uint8_t NEUN[]        = { 30, 31, 32, 33, 0xff };
-const uint8_t PM[]          = { 115, 0xff };
-const uint8_t WECKER[]      = { 114, 0xff };
-const uint8_t MIN_1[]       = { 0, 0xff };
-const uint8_t MIN_2[]       = { 1, 0xff };
-const uint8_t MIN_3[]       = { 2, 0xff };
-const uint8_t MIN_4[]       = { 3, 0xff };
+
+#ifdef LAYOUT_2020
+  const uint8_t ES[]          = { 103, 104, 0xff };
+  const uint8_t IST[]         = { 106, 107, 108, 0xff };
+  const uint8_t FUENF_M[]     = { 110, 111, 112, 113, 0xff };
+  const uint8_t ZEHN_M[]      = { 102, 101, 100, 99, 0xff };
+  const uint8_t ZWANZIG[]     = { 98, 97, 96, 95, 94, 93, 92, 0xff };
+  const uint8_t DREIVIERTEL[] = { 81, 82, 83, 84, 85, 86, 87, 88, 89 ,90, 91, 0xff };
+  const uint8_t VIERTEL[]     = { 85, 86, 87, 88, 89, 90, 91, 0xff };
+  const uint8_t VOR[]         = { 74, 73, 72, 0xff };
+  const uint8_t NACH[]        = { 78, 77, 76, 75, 0xff };
+  const uint8_t HALB[]        = { 59, 60, 61, 62, 0xff };
+  const uint8_t ELF[]         = { 34, 35, 36, 0xff };
+  const uint8_t EIN[]         = { 56, 55, 54, 0xff };
+  const uint8_t EINS[]        = { 56, 55, 54, 53, 0xff };
+  const uint8_t ZWEI[]        = { 58, 57, 56, 55, 0xff };
+  const uint8_t DREI[]        = { 38, 39, 40, 41, 0xff };
+  const uint8_t FUENF[]       = { 44, 45, 46, 47, 0xff };
+  const uint8_t VIER[]        = { 26, 27, 28, 29, 0xff };
+  const uint8_t SECHS[]       = { 9, 10, 11, 12, 13, 0xff };
+  const uint8_t ACHT[]        = { 16, 17, 18, 19, 0xff };
+  const uint8_t SIEBEN[]      = { 53, 52, 51, 50, 49, 48, 0xff };
+  const uint8_t ZWOELF[]      = { 64, 65, 66, 67, 68, 0xff };
+  const uint8_t ZEHN[]        = { 20, 21, 22, 23, 0xff };
+  const uint8_t UHR[]         = { 4, 5, 6, 0xff };
+  const uint8_t NEUN[]        = { 30, 31, 32, 33, 0xff };
+  const uint8_t PM[]          = { 115, 0xff };
+  const uint8_t WECKER[]      = { 114, 0xff };
+  const uint8_t MIN_1[]       = { 0, 0xff };
+  const uint8_t MIN_2[]       = { 1, 0xff };
+  const uint8_t MIN_3[]       = { 2, 0xff };
+  const uint8_t MIN_4[]       = { 3, 0xff };
+#endif
+
+#ifdef LAYOUT_2012
+  const uint8_t ES[]          = { 103, 104, 0xff };
+  const uint8_t IST[]         = { 106, 107, 108, 0xff };
+  const uint8_t FUENF_M[]     = { 110, 111, 112, 113, 0xff };
+  const uint8_t ZEHN_M[]      = { 102, 101, 100, 99, 0xff };
+  const uint8_t ZWANZIG[]     = { 98, 97, 96, 95, 94, 93, 92, 0xff };
+  const uint8_t DREIVIERTEL[] = { 81, 82, 83, 84, 85, 86, 87, 88, 89 ,90, 91, 0xff };
+  const uint8_t VIERTEL[]     = { 85, 86, 87, 88, 89, 90, 91, 0xff };
+  const uint8_t VOR[]         = { 80, 79, 78, 0xff };
+  const uint8_t NACH[]        = { 73, 72, 71, 70, 0xff };
+  const uint8_t HALB[]        = { 59, 60, 61, 62, 0xff };
+  const uint8_t ELF[]         = { 64, 65, 66, 0xff };
+  const uint8_t EIN[]         = { 58, 57, 56, 0xff };
+  const uint8_t EINS[]        = { 58, 57, 56, 55, 0xff };
+  const uint8_t ZWEI[]        = { 51, 50, 49, 48, 0xff };
+  const uint8_t DREI[]        = { 37, 38, 39, 40, 0xff };
+  const uint8_t FUENF[]       = { 66, 67, 68, 69, 0xff };
+  const uint8_t VIER[]        = { 44, 45, 46, 47, 0xff };
+  const uint8_t SECHS[]       = { 36, 35, 34, 33, 32, 0xff };
+  const uint8_t ACHT[]        = { 29, 28, 27, 26, 0xff };
+  const uint8_t SIEBEN[]      = { 15, 16, 17, 18, 19, 20, 0xff };
+  const uint8_t ZWOELF[]      = { 21, 22, 23, 24, 25, 0xff };
+  const uint8_t ZEHN[]        = { 14, 13, 12, 11, 0xff };
+  const uint8_t UHR[]         = { 4, 5, 6, 0xff };
+  const uint8_t NEUN[]        = { 11, 10, 9, 8, 0xff };
+  const uint8_t PM[]          = { 115, 0xff };
+  const uint8_t WECKER[]      = { 114, 0xff };
+  const uint8_t MIN_1[]       = { 0, 0xff };
+  const uint8_t MIN_2[]       = { 1, 0xff };
+  const uint8_t MIN_3[]       = { 2, 0xff };
+  const uint8_t MIN_4[]       = { 3, 0xff };
+#endif
+
 
 /**
  * Cycle through the color palette.
