8c13742ddaaf1f7a1f36bde8a8d6476eab6e3691
Thomas Smits <thomsmits@gmail.com>
2020-02-06 08:56:21 +0100

    Added function documentation

--- neoclock/ambientlight.ino
+++ neoclock/ambientlight.ino
@@ -23,17 +23,24 @@
 // darkness
 #define NO_AMBIENT_LIGHT 0.0
 
+/**
+ * Return the measured value of the ambient light
+ * sensor. If no sensor is present (PIN_LIGHTSENSOR no set)
+ * the function returns 1.0.
+ *
+ * @return the measured value in the range of 0.0 and 1.0.
+ */
 double get_ambient_brightness() {
   int value;
   #ifdef PIN_LIGHTSENSOR
     value = analogRead(PIN_LIGHTSENSOR);
-  #else 
+  #else
     return 1.0;
   #endif
-  
+
   trace("get_ambient_brightness: value=");
   trace(value);
-  
+
   double scaledValue;
 
   if (NO_AMBIENT_LIGHT < FULL_AMBIENT_LIGHT) {
@@ -41,15 +48,15 @@ double get_ambient_brightness() {
   } else {
     scaledValue = (double) ( (double)value -  FULL_AMBIENT_LIGHT) /  NO_AMBIENT_LIGHT;
   }
-  
+
   trace(", scaledValue=");
   tracenl(scaledValue);
-  
+
   // ensure that the value does never exceed 255 even if the
   // analog in port delivers a value greater than NO_AMBIENT_LIGHT
   if (scaledValue > 1) {
     scaledValue = 1;
   }
-  
+
   return scaledValue;
 }
